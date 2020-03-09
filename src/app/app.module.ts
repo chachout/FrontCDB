a996e39798d9e2c60cd20406f28456058129abc7
chachout <pierre.chabert@ecam-rennes.com>
2020-03-09 15:13:40 +0100

    Base front avec model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComputerComponent } from './computer/computer.component';
+import { CompanyComponent } from './company/company.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComputerComponent,
+    CompanyComponent
   ],
   imports: [
     BrowserModule,
