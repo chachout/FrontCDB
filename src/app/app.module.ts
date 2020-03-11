5cd12e2055e684d165ade43e9eeb52543e2f1d16
chachout <pierre.chabert@ecam-rennes.com>
2020-03-11 17:06:36 +0100

    Implémentations-des-services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './accueil/accueil.component';
 import { HeaderComponent } from './header/header.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { FormulaireComponent } from './formulaire/formulaire.component';
+
 
 
 @NgModule({
@@ -20,7 +22,8 @@ import { FormulaireComponent } from './formulaire/formulaire.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   bootstrap: [
     AppComponent
