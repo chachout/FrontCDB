1d180299581c7a5ef8592c116b941e8c24bf06a3
chachout <pierre.chabert@ecam-rennes.com>
2020-03-11 11:38:40 +0100

    Affichage-dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AccueilComponent} from './accueil/accueil.component';
 import {CommonModule} from '@angular/common';
+import {DashboardComponent} from './dashboard/dashboard.component';
 
 
 const routes: Routes = [
   {
     path: 'accueil',
     component: AccueilComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
     pathMatch: 'full'
   },
   {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './accueil/accueil.component';
 import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AccueilComponent,
-    HeaderComponent
+    HeaderComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
