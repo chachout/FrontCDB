03350497f8697125f7d723b9d1aee9d1b418818a
Issa <binkeissa16@gmail.com>
2020-03-16 15:51:35 +0100

    ajout de connexion

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../model/User.model';
+import {FormControl, FormGroup} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {ServiceLogin} from '../service/login-service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  user: User;
+  userConnected: User;
+  constructor(private loginService: ServiceLogin, private router: Router ) { }
 
   ngOnInit(): void {
+    // tslint:disable-next-line:no-unused-expression
+    this.user = new User();
+    this.loginForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl()
+    });
   }
+  login() {
+    this.user.username = this.loginForm.get('username').value;
+    this.user.password = this.loginForm.get('password').value;
+    this.seConnecter(this.user);
+  }
+  seConnecter(user: User) {
+    // tslint:disable-next-line:no-shadowed-variable
+    this.loginService.getUser(user).subscribe(user => {
+      this.userConnected = user;
+      console.log(this.userConnected);
+      // @ts-ignore
+      sessionStorage.setItem( 'username' , this.userConnected.username );
+      sessionStorage.setItem( 'password' , this.userConnected.password );
+      sessionStorage.setItem( 'role' , this.userConnected.role );
+    }, error => console.log(error)) ;
+  }
+
 
 }
