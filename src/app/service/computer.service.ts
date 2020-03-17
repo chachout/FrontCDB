9e1964594a7e35cf95c9a24eb2720ddc94a0fa16
Excilys <binkeissa16@gmail.com>
2020-03-17 12:14:23 +0100

    Feature/tri

--- src/app/service/computer.service.ts
+++ src/app/service/computer.service.ts
@@ -9,24 +9,32 @@ import {Observable, of} from 'rxjs';
 })
 export class ComputerService {
 
+  baseURL = 'http://localhost:8080/webModule';
+  
   constructor(private http: HttpClient) { }
 
   getComputers(): Observable<Computer[]> {
-    return this.http.get<Computer[]>('http://10.0.1.154:8080/webModule/computers');
+    return this.http.get<Computer[]>(this.baseURL+'/computers');
   }
   getComputer(id: string): Observable<Computer> {
-    return this.http.get<Computer>('http://10.0.1.154:8080/webModule/computer?id=' + id);
+    return this.http.get<Computer>(this.baseURL+'/computer?id=' + id);
   }
   addComputer(computer: Computer): Observable<string>  {
-    return this.http.post<string>('http://10.0.1.154:8080/webModule/addComputer', computer);
+    return this.http.post<string>(this.baseURL+'/addComputer', computer);
   }
   deleteComputer(idlist: string): Observable<void> {
-    return this.http.delete<void>('http://10.0.1.154:8080/webModule/computers/' + idlist);
+    return this.http.delete<void>(this.baseURL+'/computers/' + idlist);
   }
   updateComputer(computer: Computer): Observable<Computer> {
-    return this.http.patch<Computer>('http://10.0.1.154:8080/webModule/editComputer', computer);
+    return this.http.patch<Computer>(this.baseURL+'/editComputer', computer);
   }
   getCompanies(): Observable<Company[]> {
-    return this.http.get<Company[]>('http://10.0.1.154:8080/webModule/companies');
+    return this.http.get<Company[]>(this.baseURL+'/companies');
+  }
+  sortBy(field: string): Observable<Computer[]> {
+    return this.http.get<Computer[]>(this.baseURL+'/computers?orderBy='+field);
+  }
+  search(searchBox: string):Observable<Computer[]> {
+    return this.http.get<Computer[]>(this.baseURL+'/computers?search='+searchBox);
   }
 }
