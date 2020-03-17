3f4e4e0b2f50b7c33e26dddf75ce690e028632c7
Excilys <binkeissa16@gmail.com>
2020-03-17 13:07:08 +0100

    Feature/filter

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -11,23 +11,27 @@ import {ActivatedRoute, Router} from '@angular/router';
 export class DashboardComponent implements OnInit {
   computerList: Computer[];
   addMode = false;
-  searchBox: string ;
+  searchBox: any ;
   constructor(private computerService: ComputerService) { }
 
   ngOnInit() {
-    this.searchBox='';
+    this.searchBox = '';
     this.getComputerLIst();
   }
+
   getComputerLIst(){
     this.computerService.getComputers().subscribe(computerList => this.computerList = computerList, error => console.log(error));
   }
+
   supprimer(idList: string) {
     this.computerService.deleteComputer(idList).subscribe();
   }
+
   sort(field:string){
     console.log(field);
     this.computerService.sortBy(field).subscribe(computerList => this.computerList = computerList, error => console.log(error));
   }
+
   search(){
     this.computerService.search(this.searchBox).subscribe(computerList => this.computerList = computerList, error => console.log(error));
   }
